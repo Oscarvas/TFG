c33a5f8a475ff1845d4b9f3c09e8d105e0dda64d
Oscar Vasquez <oscardavid.vp@gmail.com>
2016-03-21 20:57:58 +0100

    Paquete de objetos. Ha recibido alguna modificacion respecto al original

---
+++ src/objetos/Objeto.java
@@ -0,0 +1,26 @@
+package objetos;
+
+public abstract class Objeto {
+	private String id;
+	private String descripcion;
+	
+	protected Objeto (String id, String descripcion) {
+		this.id = id;
+		this.descripcion = descripcion;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String toString() {
+		String cadena = "";
+		
+		cadena += "-- objeto[ " + this.id + " ]= " + this.descripcion;
+		return cadena;
+	}
+	
+//	public abstract boolean usar(Personaje quien, Localizacion donde);
+	public abstract boolean puedeUsarse();
+	public abstract boolean puedeCogerse();
+}
