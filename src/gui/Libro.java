41537885b2a08777822c19f8365056e97c79dce0
Oscar Vasquez <oscardavid.vp@gmail.com>
2016-03-22 21:17:12 +0100

    Panel con el fondo del libro. Se usara este panel para escribir la historia generada sobre el.

---
+++ src/gui/Libro.java
@@ -0,0 +1,19 @@
+package gui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.border.EmptyBorder;
+
+public class Libro extends JPanelBackground {
+
+	/**
+	 * Create the panel.
+	 */
+	public Libro() {
+		super();
+		this.setBackground("resources/images/bookBkgrd.jpg");
+		this.setBorder(new EmptyBorder(5, 5, 5, 5));
+		this.setLayout(new BorderLayout(0, 0));
+	}
+
+}
