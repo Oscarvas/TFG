49ce9cbe06a06f84941c3aa368bc97f883171991
Oscarvas <odvperaza@ucm.es>
2016-03-29 20:11:53 +0200

    Copias proyecto anterior

--- src/personajes/Dragon.java
+++ src/personajes/Dragon.java
@@ -1,11 +1,175 @@
 package personajes;
 
+import java.util.Random;
+
 import gui.Gui;
+import jade.core.AID;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 @SuppressWarnings("serial")
 public class Dragon extends Personaje {
+	public AID princesaSecuestrada;
+
 	protected void setup(){
 		iniciarMonstruo();
-		Gui.setHistoria("El imponente rugido del dragón "+getLocalName()+" se escucha por todo el reino.");
+		Gui.setHistoria("Desde "+getLocalizacion()+ ", el imponente rugido del dragón "+getLocalName()+" se escucha por todo el reino.");
+		addBehaviour(new Secuestro());
 	}
+	protected void takeDown() {
+		
+		if ( estaMuerto() )
+			Gui.setHistoria("# El dragón " + getAID().getLocalName() + " ha muerto en batalla. \n");
+		
+		else
+			Gui.setHistoria("# El dragón " + getAID().getLocalName() + " se retira. \n");
+			
+	}
+	
+	
+	private class Secuestro extends Behaviour {
+			
+			boolean ok;
+			
+			public void action() {
+				
+				DFAgentDescription template = new DFAgentDescription();
+				ServiceDescription sd = new ServiceDescription();
+				sd.setType("Secuestrable");
+				template.addServices(sd);
+						
+				try{
+					DFAgentDescription[] result = DFService.search(myAgent, template);
+					//Gui.setHistoria("# Las siguientes princesas son secuestrables:");
+					AID[] PrincesasSecuestrables = new AID[result.length];
+					for (int i = 0; i < result.length; i++){
+						PrincesasSecuestrables[i] = result[i].getName();
+						//System.out.println("\t" + PrincesasSecuestrables[i].getLocalName());
+					}
+					System.out.println();
+					
+					if (PrincesasSecuestrables.length != 0) {
+						princesaSecuestrada = PrincesasSecuestrables[new Random().nextInt(PrincesasSecuestrables.length)];
+						planificar();
+						addBehaviour(new FalloSecuestro());
+						addBehaviour(new FinPlanificacion());
+						
+						ok = true;
+						
+					} else {
+						Thread.sleep(3000);
+						reset();
+					}
+							
+				} catch (Exception e){ 
+					e.printStackTrace();
+				}
+						
+				if( !done() )
+					reset();
+			}
+	
+			@Override
+			public boolean done() {
+				return ok;
+			}
+		}		
+	
+	private class FinPlanificacion extends Behaviour {
+		
+		ACLMessage receive;
+		
+		public void action() {
+	
+			MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Fin-Plan"),
+					MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+			
+			receive = receive(mt);
+			
+			if ( receive != null ) {
+				ACLMessage secuestrar = new ACLMessage(ACLMessage.INFORM);
+				secuestrar.setConversationId("Te secuestro");
+				secuestrar.addReceiver(princesaSecuestrada);
+		
+				myAgent.send(secuestrar);
+				
+				MessageTemplate mt1 = MessageTemplate.MatchConversationId("Te secuestro");
+				myAgent.blockingReceive(mt1);
+			
+				addBehaviour(new Defender());
+				
+			} else
+				block();
+		}
+	
+		@Override
+		public boolean done() {
+			return receive != null;
+		}
+	}
+	
+	
+	private class FalloSecuestro extends Behaviour {
+		
+		ACLMessage receive;
+		
+		public void action() {
+			
+			MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Fallo Secuestro"),
+					MessageTemplate.MatchPerformative(ACLMessage.FAILURE));
+			
+			receive = receive(mt);
+			
+			if ( receive != null ) {
+				
+				try {
+					
+					Thread.sleep(3000);
+					myAgent.addBehaviour(new Secuestro());
+					
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			
+			} else
+				block();
+		}
+	
+		public boolean done() {
+			return receive != null;
+		}
+	}
+		
+	private class Defender extends CyclicBehaviour {
+			
+			ACLMessage receive;
+			
+			@Override
+			public void action() {
+				
+				MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Batalla"),
+						MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+				receive = myAgent.receive(mt);
+				
+				if ( receive != null ) {
+					ACLMessage reply = receive.createReply();
+						
+					reply.setContent(Integer.toString(getVida()));
+					añadirVida(-Integer.parseInt(receive.getContent()));				
+					
+					myAgent.send(reply);
+					
+					if ( estaMuerto() )
+						doDelete();
+					
+				} else
+					block();
+			}
+		}
+
 }
--- src/personajes/Princesa.java
+++ src/personajes/Princesa.java
@@ -1,10 +1,22 @@
 package personajes;
 
 import gui.Gui;
+import jade.core.AID;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import ontologia.Mitologia;
 
 @SuppressWarnings("serial")
 public class Princesa extends Personaje {
+	private AID padre;
+	public AID dragon;
+
 	protected void setup(){
 		Object[] args = getArguments(); 
 		if (args != null && args.length > 0) {
@@ -13,7 +25,116 @@ public class Princesa extends Personaje {
 					Integer.parseInt((String) args[4]), Integer.parseInt((String) args[5]), false);
 		}
 		localizarPersonaje();
-		mandarCrearArchivo();
 		Gui.setHistoria("En un arrebato de rebeldía, la princesa "+getLocalName()+" ha llegado hasta "+getLocalizacion()+".");
+		
+		padre = getAID("Felipe");
+
+		
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Secuestrable");
+		sd.setName(getLocalName()+"-Secuestrable");
+		dfd.addServices(sd);
+		
+		try{
+			DFService.register(this, dfd);
+		} catch (FIPAException fe){
+			fe.printStackTrace();
+		}
+		
+		addBehaviour(new MoverSecuestrada());
+		addBehaviour(new AvisaAPadre());
+		addBehaviour(new Rescatada());
 	}
+	
+	protected void takeDown() {
+		Gui.setHistoria("* La Princesa " + getLocalName() + " pone fin a su aventura. \n");
+	}
+	
+	private class MoverSecuestrada extends CyclicBehaviour {
+		
+		public void action() {
+			
+			MessageTemplate mt = MessageTemplate.and(
+					MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
+					MessageTemplate.MatchConversationId("Mover-Princesa"));
+			ACLMessage receive = myAgent.receive(mt);
+			
+			if ( receive != null ) {
+
+				moverSecuestrado(receive.getContent());
+				ACLMessage reply = receive.createReply();
+				myAgent.send(reply);	
+				
+			} else
+				block();
+			
+		}
+	}
+	
+	private class AvisaAPadre extends Behaviour {
+
+		private ACLMessage receive;	
+		
+		public void action() {
+			
+			MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Te secuestro"),
+					MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+			receive = receive(mt);
+			
+			if ( receive != null ) {
+				
+				try{
+					DFService.deregister(myAgent);
+				}catch (FIPAException fe){
+					fe.printStackTrace();
+				}
+				
+				dragon = receive.getSender();
+				send(receive.createReply());	
+			
+				Gui.setHistoria("* La Princesa " + myAgent.getLocalName() + " ha sido secuestrada. \n");
+				
+				ACLMessage inform = new ACLMessage(ACLMessage.REQUEST);
+				inform.setConversationId("Ayuda");
+				inform.setReplyWith("request" + System.currentTimeMillis());
+				inform.addReceiver(padre);
+				inform.setContent(dragon.getLocalName());
+				myAgent.send(inform);
+
+			} else
+				block();
+		}
+
+		@Override
+		public boolean done() {
+			return receive != null;
+		}
+	}
+	
+	private class Rescatada extends Behaviour {
+		
+		ACLMessage receive;
+		
+		public void action() {
+			
+			MessageTemplate mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Rescatada"),
+					MessageTemplate.MatchPerformative(ACLMessage.INFORM));
+			receive = receive(mt);
+			
+			if ( receive != null ) {
+				
+				myAgent.doDelete();
+				
+			} else
+				block();
+			
+		}
+		
+		public boolean done() {
+			return receive != null;
+		}
+	}
+
 }
