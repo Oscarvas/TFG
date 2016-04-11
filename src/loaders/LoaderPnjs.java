d4e2ecb77d14df84cef0cbe6c3420bc9ae77e210
JuanOre <usuario_local@PTO1204>
2016-04-11 20:21:12 +0200

    Cambios para configurar correctamente pnjs.
    Falta por retocar la clase pnjs para que se creen correctamente.
    Ver compatibilidad con la clase personaje.

--- src/loaders/LoaderPnjs.java
+++ src/loaders/LoaderPnjs.java
@@ -1,5 +1,15 @@
 package loaders;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import jade.wrapper.AgentController;
 import jade.wrapper.ControllerException;
 import jade.wrapper.PlatformController;
@@ -7,22 +17,61 @@ import mundo.Mundo;
 
 public class LoaderPnjs {
 	public LoaderPnjs(Mundo mundo) throws ControllerException{
+		String nombre;
+		String oficio;
+		String localizacion;
 		PlatformController container = mundo.getContainerController();
 		
 		AgentController guest;
-		guest = container.createNewAgent("Thrall", "pnjs.Chaman", null);
-		guest.start();
-		guest = container.createNewAgent("Kvothe", "pnjs.Tabernero", null);
-		guest.start();
-		guest = container.createNewAgent("Egidio", "pnjs.Granjero", null);
-		guest.start();
-		guest = container.createNewAgent("Chicote", "pnjs.Chef", null);
-		guest.start();
-		guest = container.createNewAgent("Bella", "pnjs.Bibliotecario", null);
-		guest.start();
-		guest = container.createNewAgent("Felurian", "pnjs.Sastre", null);
-		guest.start();
-		guest = container.createNewAgent("Garrosh", "pnjs.Herrero", null);
-		guest.start();
+		
+		
+		try {
+			//indicar el fichero de configuracion y crear el builder
+			File fXmlFile = new File("PNJs.xml");
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			doc.getDocumentElement().normalize();
+			
+			//volcar en una lista de nodos los pnjs que se quieren coger
+			NodeList nList = doc.getElementsByTagName("pnj");
+			//crear un numero n de pnjs a hacer
+			for (int n = 0; n < nList.getLength(); n++) {
+
+				Node nNode = nList.item(n);
+
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElement = (Element) nNode;
+
+					nombre = eElement.getAttribute("nombre");
+					String[] args = { eElement.getAttribute("oficio"),eElement.getAttribute("localizacion")};
+					
+					guest = container.createNewAgent(nombre, "pnjs.PNJ", args);
+					guest.start();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+//
+//		guest = container.createNewAgent("Thrall", "pnjs.Chaman", null);
+//		guest.start();
+//		guest = container.createNewAgent("Kvothe", "pnjs.Tabernero", null);
+//		guest.start();
+//		guest = container.createNewAgent("Egidio", "pnjs.Granjero", null);
+//		guest.start();
+//		guest = container.createNewAgent("Chicote", "pnjs.Chef", null);
+//		guest.start();
+//		guest = container.createNewAgent("Bella", "pnjs.Bibliotecario", null);
+//		guest.start();
+//		guest = container.createNewAgent("Felurian", "pnjs.Sastre", null);
+//		guest.start();
+//		guest = container.createNewAgent("Garrosh", "pnjs.Herrero", null);
+//		guest.start();
 	}
 }
---
+++ src/pnjs/PNJ.java
@@ -0,0 +1,100 @@
+package pnjs;
+
+import ontologia.Mitologia;
+import acciones.OfrecerServicios;
+import gui.Gui;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class PNJ extends Agent {
+	protected void setup(){
+		Object[] args = getArguments(); 
+//		if (args != null && args.length > 0) {
+//			iniciarPrincipal(Mitologia.valueOf((String) args[0]), Integer.parseInt((String) args[1]), 
+//					Integer.parseInt((String) args[2]), Integer.parseInt((String) args[3]), 
+//					Integer.parseInt((String) args[4]), Integer.parseInt((String) args[5]), false);
+//		}
+		
+		//aqui debemos coger los args que hacemos al crear el pnj [0]oficio [1]localizacion
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType((String) args[0]);
+		sd.setName(getLocalName()+(String) args[0]);
+		dfd.addServices(sd);
+		
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		
+		
+//		localizarPnj();
+//		Gui.setHistoria("El caballero "+getLocalName()+" se ha despertado en "+getLocalizacion()+" con su armadura hecha polvo.");
+//
+//		addBehaviour(new OfrecerServicios(getTesoro()));		
+//		addBehaviour(new AceptarOfertaRescate());	
+//		
+//		
+//		
+//		
+//		DFAgentDescription dfd = new DFAgentDescription();
+//		dfd.setName(getAID());
+//		ServiceDescription sd = new ServiceDescription();
+//		sd.setType("Chamanismo");
+//		sd.setName(getLocalName()+"-Chamanismo");
+//		dfd.addServices(sd);
+		
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		Gui.setHistoria("El chamán "+getLocalName()+" entra en sintonía con los elementos");
+	}
+	
+//	public void localizarPnj() {
+//
+//		DFAgentDescription template = new DFAgentDescription();
+//		ServiceDescription sd = new ServiceDescription();
+//		sd.setType("Mundo");
+//		template.addServices(sd);
+//
+//		try {
+//
+////			DFAgentDescription[] result = DFService.search(this, template);
+////			setAgenteMundo(result[0].getName());
+////
+////			ACLMessage localizar = new ACLMessage(ACLMessage.REQUEST);
+////			localizar.addReceiver(getAgenteMundo());
+////			localizar.setConversationId("Mover");
+////			localizar.setContent(getClass().getName().substring(11) + " "
+////					+ localizacion);
+////			localizar.setReplyWith("localizar" + System.currentTimeMillis());
+////			send(localizar);
+//
+//			MessageTemplate mt = MessageTemplate.and(
+//					MessageTemplate.MatchConversationId("Mover"),
+//					MessageTemplate.MatchInReplyTo(localizar.getReplyWith()));
+//			blockingReceive(mt);
+//
+//		} catch (FIPAException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+	
+//	public String getLocalizacion() {
+//		return this.localizacion;
+//	}
+//
+//	public void setLocalizacion(String localizacion) {
+//		this.localizacion = localizacion;
+//	}
+}
