f62cfe7dd4d0bbf8db5afde8153467c6cdefb031
JuanOre <usuario_local@PTO0311>
2016-04-12 19:08:10 +0200

    Cambios en la construccion de pnj.
    Eliminados los comentarios.
    Cambie que se indique tambien el sexo del pnj, facilitará la
    construccion de las frases (puede que esto deba ser pensado en todos los
    pjs y monstruos).
    Ya debería funcionar la clase pnj pero no se puede probar hasta tener la
    localizacion.
    Aparte no me apaño bien para inicializarlos, cuando podamos lo probamos
    juntos.

--- src/pnjs/PNJ.java
+++ src/pnjs/PNJ.java
@@ -1,23 +1,24 @@
 package pnjs;
 
 import gui.Gui;
+import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
 @SuppressWarnings("serial")
 public class PNJ extends Agent {
+	private String localizacion;	
+	private AID agenteMundo;	
+	
 	protected void setup(){
 		Object[] args = getArguments(); 
-//		if (args != null && args.length > 0) {
-//			iniciarPrincipal(Mitologia.valueOf((String) args[0]), Integer.parseInt((String) args[1]), 
-//					Integer.parseInt((String) args[2]), Integer.parseInt((String) args[3]), 
-//					Integer.parseInt((String) args[4]), Integer.parseInt((String) args[5]), false);
-//		}
 		
-		//aqui debemos coger los args que hacemos al crear el pnj [0]oficio [1]localizacion
+		//aqui debemos coger los args que hacemos al crear el pnj [0]sexo [1]oficio [2]localizacion
 		DFAgentDescription dfd = new DFAgentDescription();
 		dfd.setName(getAID());
 		ServiceDescription sd = new ServiceDescription();
@@ -30,68 +31,60 @@ public class PNJ extends Agent {
 		} catch (FIPAException fe) {
 			fe.printStackTrace();
 		}
-		Gui.setHistoria(getLocalName()+" es "+args[0]+" y esta en "+args[1]);
 		
-//		localizarPnj();
-//		Gui.setHistoria("El caballero "+getLocalName()+" se ha despertado en "+getLocalizacion()+" con su armadura hecha polvo.");
-//
-//		addBehaviour(new OfrecerServicios(getTesoro()));		
-//		addBehaviour(new AceptarOfertaRescate());	
-//		
-//		
-//		
-//		
-//		DFAgentDescription dfd = new DFAgentDescription();
-//		dfd.setName(getAID());
-//		ServiceDescription sd = new ServiceDescription();
-//		sd.setType("Chamanismo");
-//		sd.setName(getLocalName()+"-Chamanismo");
-//		dfd.addServices(sd);
-//		
-//		try {
-//			DFService.register(this, dfd);
-//		} catch (FIPAException fe) {
-//			fe.printStackTrace();
-//		}
-//		Gui.setHistoria("El chamán "+getLocalName()+" entra en sintonía con los elementos");
+		localizarPnj();
+		// Se formaran frases del estilo "El [oficio] [nombre] se levanta en [localizacion] dispuesto a trabajar
+		// "La bibliotecaria Luisa se levanta en la biblioteca con ganas de trabajar."
+		if (args[0] == "M") 
+			Gui.setHistoria("El " + args[1] + " " + getLocalName()+ " se levanta en " +args[2] + " con ganas de trabajar.");
+		else
+			Gui.setHistoria("La " + args[1] + " " + getLocalName()+ " se levanta en " +args[2] + " con ganas de trabajar.");
 	}
 	
-//	public void localizarPnj() {
-//
-//		DFAgentDescription template = new DFAgentDescription();
-//		ServiceDescription sd = new ServiceDescription();
-//		sd.setType("Mundo");
-//		template.addServices(sd);
-//
-//		try {
-//
-////			DFAgentDescription[] result = DFService.search(this, template);
-////			setAgenteMundo(result[0].getName());
-////
-////			ACLMessage localizar = new ACLMessage(ACLMessage.REQUEST);
-////			localizar.addReceiver(getAgenteMundo());
-////			localizar.setConversationId("Mover");
-////			localizar.setContent(getClass().getName().substring(11) + " "
-////					+ localizacion);
-////			localizar.setReplyWith("localizar" + System.currentTimeMillis());
-////			send(localizar);
-//
-//			MessageTemplate mt = MessageTemplate.and(
-//					MessageTemplate.MatchConversationId("Mover"),
-//					MessageTemplate.MatchInReplyTo(localizar.getReplyWith()));
-//			blockingReceive(mt);
-//
-//		} catch (FIPAException e) {
-//			e.printStackTrace();
-//		}
-//
-//	}
+	public void setAgenteMundo(AID agenteMundo) {
+		this.agenteMundo = agenteMundo;
+	}
 	
-//	public String getLocalizacion() {
-//		return this.localizacion;
-//	}
-//
-//	public void setLocalizacion(String localizacion) {
-//		this.localizacion = localizacion;
-//	}
+	public AID getAgenteMundo() {
+		return agenteMundo;
+	}
+	
+	//no entiendo bien el metodo, en principio me parece que sirve para mover pj's, los pnj's no se van a mover pero lo pongo por si acaso
+	public void localizarPnj() {
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("Mundo");
+		template.addServices(sd);
+
+		try {
+
+			DFAgentDescription[] result = DFService.search(this, template);
+			setAgenteMundo(result[0].getName());
+
+			ACLMessage localizar = new ACLMessage(ACLMessage.REQUEST);
+			localizar.addReceiver(getAgenteMundo());
+			localizar.setConversationId("Mover");
+			localizar.setContent(getClass().getName().substring(11) + " "
+					+ localizacion);
+			localizar.setReplyWith("localizar" + System.currentTimeMillis());
+			send(localizar);
+
+			MessageTemplate mt = MessageTemplate.and(
+					MessageTemplate.MatchConversationId("Mover"),
+					MessageTemplate.MatchInReplyTo(localizar.getReplyWith()));
+			blockingReceive(mt);
+
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public String getLocalizacion() {
+		return this.localizacion;
+	}
+
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
 }
