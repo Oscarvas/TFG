8e484912e74b5734d96df121d0c56fc60f7591ad
oscarvas <odvperaza@ucm.es>
2016-04-30 19:15:57 +0200

    Tipos de localizacion como constantes
    El loader de monstruos ya no requiere la lozalizacion
    
    Limpieza de codigo y warnings

--- src/ontologia/Mitologia.java
+++ src/ontologia/Mitologia.java
@@ -1,23 +1,27 @@
 package ontologia;
 
 /*
- * Enumerado de las zonas
-Esta clase guardara la referencia a la raza propia de cada region del mapa
-asi como los atributos predefinidos de cada raza y que afectaran al desarrollo de cada personaje
-durante su historia.
-Formato:
-	Raza	Zona	vida,fuerza,destreza,inteligencia,codicia	RegionesInternas
+ * String[] TIPOS_LOCALIZACION = {"Pueblo","Bosque","Urbano","Castillo","Guarida","Lago"};
+ * Pueblo = 0
+ * Bosque = 1
+ * Urbano = 2
+ * Castillo = 3
+ * Guarida = 4
+ * Lago = 5
+ * 
+ * Esta clase mantiene la asociacion entre la clase de un personaje y su localizacion predeterminada en funcion de la clase
+ * En el caso del personaje Maligno, puede aparecer en cualquier localizacion
 */
 public enum Mitologia {
-	CABALLERO("Pueblo"),
-	DRUIDA("Bosque"),
-	MAGO("Urbano"),
-	REY("Castillo"),
-	PRINCESA("Castillo"),
-	SECUESTRADOR("Guarida"),
-	EMBOSCADOR("Bosque"),
-	GUARDIAN("Lago"),
-	MALIGNO("Todas"); 
+	CABALLERO(Vocabulario.TIPOS_LOCALIZACION[0]),
+	DRUIDA(Vocabulario.TIPOS_LOCALIZACION[1]),
+	MAGO(Vocabulario.TIPOS_LOCALIZACION[2]),
+	REY(Vocabulario.TIPOS_LOCALIZACION[3]),
+	PRINCESA(Vocabulario.TIPOS_LOCALIZACION[3]),
+	SECUESTRADOR(Vocabulario.TIPOS_LOCALIZACION[4]),
+	EMBOSCADOR(Vocabulario.TIPOS_LOCALIZACION[1]),
+	GUARDIAN(Vocabulario.TIPOS_LOCALIZACION[5]),
+	MALIGNO(Vocabulario.TIPOS_LOCALIZACION[Vocabulario.LOC_MALIGNO()]); 
 	
 	private final String tipo;
 	
