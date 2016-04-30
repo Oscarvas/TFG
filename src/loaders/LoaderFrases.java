772409253288ad0e725cfa0afe0916c50fdb5541
Samuel <samuelro@ucm.es>
2016-04-30 16:46:47 +0200

    #1 LoaderFrases
    Creada clase loader para el nuevo formato de frases de los xml.

--- src/loaders/LoaderFrases.java
+++ src/loaders/LoaderFrases.java
@@ -1,55 +1,64 @@
 package loaders;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Random;
+import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import ontologia.Frases;
 
 public class LoaderFrases {
-	@SuppressWarnings("unused")
-	public LoaderFrases () {
-		try{
-			File fXmlFile = new File("Frases.xml");
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
-					.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+	public static Frases loaderFrases() {
+		Frases frases = new Frases();
+
+		File fXmlFile = new File("Frases.xml");
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(fXmlFile);
-	
+
 			doc.getDocumentElement().normalize();
-	
-			NodeList nList = doc.getElementsByTagName("personaje");
-	
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-	
-				Node nNode = nList.item(temp);
-	
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-	
-					Element eElement = (Element) nNode;
-						
-					int nFrases = eElement.getElementsByTagName("frase").getLength();
-					ArrayList<String> frases = new ArrayList<String>();
-					for(int i=0;i<nFrases;i++){
-						frases.add(eElement
-							.getElementsByTagName("frase")
-							.item(i).getTextContent());
+
+			NodeList nListPersonajes = doc.getElementsByTagName("personaje");
+			for (int i = 0; i < nListPersonajes.getLength(); i++) {
+
+				Node nNodePersonajes = nListPersonajes.item(i);
+				if (nNodePersonajes.getNodeType() == Node.ELEMENT_NODE) {
+
+					Element eElementPersonaje = (Element) nNodePersonajes;
+					NodeList nListAcciones = eElementPersonaje.getElementsByTagName("accion");
+
+					for (int j = 0; j < nListAcciones.getLength(); j++) {
+						Node nNodeAcciones = nListAcciones.item(j);
+						if (nNodeAcciones.getNodeType() == Node.ELEMENT_NODE) {
+							Element eElementAccion = (Element) nNodeAcciones;
+							NodeList nListFrases = eElementAccion.getElementsByTagName("frase");
+							for (int k = 0; k < nListFrases.getLength(); k++) {
+								Node nNodeFrases = nListFrases.item(k);
+								if (nNodeFrases.getNodeType() == Node.ELEMENT_NODE) {
+									Element eElementFrase = (Element) nNodeFrases;
+									frases.añadirFrase(eElementPersonaje.getAttribute("tipo"),
+											eElementAccion.getAttribute("id"), 
+											eElementFrase.getTextContent());
+								}
+							}
+						}
 					}
-					String cadena = frases.get(new Random().nextInt(frases.size()));
-					
 				}
 			}
-		}
-		catch (Exception e) {
+		} catch (ParserConfigurationException | SAXException | IOException e) {
 			e.printStackTrace();
-			System.exit(0);
 		}
+		return frases;
 	}
 }
