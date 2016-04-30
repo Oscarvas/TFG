41cce2bec902f8ac7fde6217c0c25703820c8467
Samuel <samuelro@ucm.es>
2016-04-30 16:54:04 +0200

    #1 Modificado clase Frase
    Modificado para que tenga un metodo que permita guardar las frases que
    se cargan

--- src/ontologia/Frases.java
+++ src/ontologia/Frases.java
@@ -10,9 +10,17 @@ public class Frases {
 	public HashMap<String, HashMap<String, ArrayList<String>>> getFrases() {
 		return frases;
 	}
-
-	public void setFrases(HashMap<String, HashMap<String, ArrayList<String>>> frases) {
-		this.frases = frases;
-	}	
 	
+	public void añadirFrase(String personaje, String accion, String frase){
+		if(this.frases.get(personaje) == null){
+			this.frases.put(personaje, new HashMap<String, ArrayList<String>>());
+		}
+		if(this.frases.get(personaje).get(accion) == null){
+			this.frases.get(personaje).put(accion, new ArrayList<String>());
+		}
+		ArrayList<String> aux = this.frases.get(personaje).get(accion);
+		aux.add(frase);
+		this.frases.get(personaje).put(accion, aux);
+	}
+
 }
