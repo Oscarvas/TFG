d27db40c106f96e052e12f75a7802fa780f1d7e9
oscarvas <odvperaza@ucm.es>
2016-08-10 23:37:45 +0200

    Daño realizado por el asesino aleatorizado

--- src/acciones/Agonia.java
+++ src/acciones/Agonia.java
@@ -1,5 +1,7 @@
 package acciones;
 
+import java.util.Random;
+
 import gui.Gui;
 import jade.core.AID;
 import jade.lang.acl.ACLMessage;
@@ -28,7 +30,7 @@ public class Agonia {
 		/*
 		 * Enviamos como mensaje la cantidad de vida a perder por el personaje afectado
 		 * */
-		agonia.setContent(Integer.toString(15));
+		agonia.setContent(Integer.toString(new Random().nextInt(personaje.getVida()+1)));
 		
 		Gui.setHistoria(personaje.getLocalName()+ ": " + secundario + ", ahora sentirás la agonía de una muerte lenta");
 		
